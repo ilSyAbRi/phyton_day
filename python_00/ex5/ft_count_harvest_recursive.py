6a0de1ea2fb764c1423a1ce5427caf71b7c5d4eb
ilSyAbRi <iliasssyabri1@gmail.com>
2026-02-04 19:40:08 +0100

    do it whit recursion in the correct way

--- python_00/ex5/ft_count_harvest_recursive.py
+++ python_00/ex5/ft_count_harvest_recursive.py
@@ -1,8 +1,11 @@
 def ft_count_harvest_recursive():
-    n = int(input("Days until harvest ")):
-        ft_rec(day):
-            if (day > n):
-                return
-            else:
-                print("day ",day):
+    n = int(input("Days until harvest "))
+
+    def ft_rec(day):
+        if (day > n):
+            print("Harvest time!")
+            return
+        else:
+            print("DAY ",day)
+            ft_rec(day + 1)
     ft_rec(1)
